159263646c3fb224064a0c343054c697da277202
Emilio Barbosa <emilio.projetos@gmail.com>
2023-12-19 09:29:42 -0300

    capacidade de cada variavel

---
+++ src/main/kotlin/Capacidade das variaveis.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // para determinar a capacidade para variavel UNSIGNED temos://
+    println("Capacidade maxima do ULong: ${ULong.MAX_VALUE} Capacidade minima do Ulong: ${ULong.MIN_VALUE}")//ULong - tamanho de uma variavel Unsigned
+    println("Capacidade maxima do Long: ${Long.MAX_VALUE} Capacidade minima do Long: ${Long.MIN_VALUE}")
+    println("Capacidade maxima do Int: ${Int.MAX_VALUE} Capacidade minima do Int: ${Int.MIN_VALUE}") // Para variavel sem modulo abrange para o lado negativo ate o positivo ex: -198252974618756980 a 198252974618756980.
+    println("Capacidade maxima do Short: ${Short.MAX_VALUE} Capacidade minima do Short: ${Short.MIN_VALUE}")
+    println("Capacidade maxima do Float: ${Float.MAX_VALUE} Capacidade minima do Float: ${Float.MIN_VALUE}")
+    println("Capacidade maxima do Char: ${Char.MAX_VALUE} Capacidade minima do Char: ${Char.MIN_VALUE}")
+    println("Capacidade maxima do Byte: ${Byte.MAX_VALUE} Capacidade minima do Byte: ${Byte.MIN_VALUE}")
+
+
+
+
+
+}
