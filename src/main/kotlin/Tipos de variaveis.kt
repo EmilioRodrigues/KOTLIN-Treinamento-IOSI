86781c00ee6993038c02f5e7073dc1f93f1a0d46
Emilio Barbosa <emilio.projetos@gmail.com>
2023-12-19 09:00:45 -0300

    imprimindo as variáveis

--- src/main/kotlin/Tipos de variaveis.kt
+++ src/main/kotlin/Tipos de variaveis.kt
@@ -1,16 +1,28 @@
 fun main (){
     // todas os tipos de variaveis sao iniciados por letra maiuscula//
-    var x: Int // var
-    val y: Int
-    var a: Float // numeros decimais
-    var b: Double // mais numeros decimais que o float
-    var c: Long // menos usado mas abrange quantidade maior de numeros
-    var d: Char // para letras
-    var e: String // comum em todas as linguagens, sao as frases
-    var f: Short // pouco utilizado - numeros menores
-    var g: Byte // armazena formatos numericos tipos binarios e hexadecimais
+    var x: Int = 10 // var
+    val y: Int = 12
+    var a: Float = 19.76f // numeros decimais e é obrigatorio acrescentar a letra 'f' depois do numero para que o codigo esteja correto;//
+    var b: Double = 3.847626859// mais numeros decimais que o float e nao precisa acrescentar o 'f'.
+    var c: Long = 3850622985959189099 // menos usado mas abrange quantidade maior de numeros limitado a 19 numeros
+    var d: Char = 's' // para letras sempre com ASPAS SIMPLES
+    var e: String = "Emilio" // comum em todas as linguagens, sao as frases sempre entre ASPAS DUPLAS
+    var f: Short = 199 // pouco utilizado - numeros menores INTEIROS
+    var g: Byte = 0b1010// armazena formatos numericos tipos binarios, inicia com 0 seguido da letra 'b' e depois a representação em binario do numero desejado
+    var h: Byte = 0x10 // armazena formatos numericos tipos  hexadecimais, inicia com 0 seguido da letra 'x' e depois a representação em hexadecimal do numero desejado
+    var i: UInt = 12u //UNSIGNED -  indica o modulo do numero inserido na variavel, numero seguido da letra 'u'.
 
-
+    println(x)
+    println(y)
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+    println(i)
 
 
 }
