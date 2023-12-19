341beaf3fc26f3e4359134690f1b2b4b138756b4
Emilio Barbosa <emilio.projetos@gmail.com>
2023-12-19 08:29:41 -0300

    segundo commit

--- src/main/kotlin/Tipos de variaveis.kt
+++ src/main/kotlin/Tipos de variaveis.kt
@@ -1,12 +1,13 @@
 fun main (){
-    var x: Int
+    // todas os tipos de variaveis sao iniciados por letra maiuscula//
+    var x: Int // var
     val y: Int
-    var a: Float
-    var b: Double
-    var c: Long
-    var d: Char
-    var e: String
-    var f: Short //
+    var a: Float // numeros decimais
+    var b: Double // mais numeros decimais que o float
+    var c: Long // menos usado mas abrange quantidade maior de numeros
+    var d: Char // para letras
+    var e: String // comum em todas as linguagens, sao as frases
+    var f: Short // pouco utilizado - numeros menores
     var g: Byte // armazena formatos numericos tipos binarios e hexadecimais
 
 
