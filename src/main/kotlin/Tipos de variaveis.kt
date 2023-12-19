ae43d7bd472c91d3c2cabcb3c2b200a24d261932
Emilio Barbosa <emilio.projetos@gmail.com>
2023-12-19 06:42:53 -0300

    Initial commit

---
+++ src/main/kotlin/Tipos de variaveis.kt
@@ -0,0 +1,15 @@
+fun main (){
+    var x: Int
+    val y: Int
+    var a: Float
+    var b: Double
+    var c: Long
+    var d: Char
+    var e: String
+    var f: Short //
+    var g: Byte // armazena formatos numericos tipos binarios e hexadecimais
+
+
+
+
+}
