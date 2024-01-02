3fe6df10d0649374efc717f0aa0bc504048ff2f0
Emilio Barbosa <emilio.projetos@gmail.com>
2024-01-02 17:30:54 -0300

    Estrutura de repetição FOR

---
+++ src/main/kotlin/Estrutura de repeticao FOR.kt
@@ -0,0 +1,28 @@
+fun main() {
+    for (i in 1..20) { //estrutura de repetição FOR crescente  de 1 em 1.
+        print("${i} ")
+    }
+
+    print("\n")
+
+    for (i in 20 downTo 1){ //estrutura de repetição FOR decrescente de 1 em 1.
+        print("${i} ")
+    }
+
+    print("\n")
+    val str = "Criação de Aplicativos Android"
+    for (i in str){
+        print("${i} ")
+    }
+    print("\n")
+    for (i in 1..20 step 3) { //estrutura de repetição FOR crescente  de 1 em 1.
+        print("${i} ")}
+
+    print("\n")
+
+    for (i in 20 downTo 1 step 3){ //estrutura de repetição FOR decrescente de 1 em 1.
+        print("${i} ")}
+
+}
+
+
